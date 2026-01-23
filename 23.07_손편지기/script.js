e4f94d1fbba640456590fd45c29ccc996359132d
hjn-kim <127818850+hjn-kim@users.noreply.github.com>
2026-01-23 12:41:10 +0900

    Add files via upload

--- 23.07_손편지기/script.js
+++ 23.07_손편지기/script.js
@@ -61,7 +61,7 @@ function sendLetter() {
       alert("편지 보내기 오류: " + error.message);
     });
 }
-const correctPassword = "";
+const correctPassword = "8999";
 
 function loadSavedData() {
   // 모달 창 표시
@@ -107,4 +107,3 @@ function checkPassword() {
     alert("비밀번호가 틀렸습니다.");
   }
 }
-
